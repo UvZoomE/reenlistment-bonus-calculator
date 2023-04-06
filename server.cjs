07d78b173e98c0db1108e5ed9793d75b7c1f65fa
Uvaldo Zumaya <uvaldo.zumaya@spaceforce.mil>
2023-04-06 17:31:29 -0600

    Fixed scripts to run vite on Heroku

--- server.cjs
+++ server.cjs
@@ -2,10 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
